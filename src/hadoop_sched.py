db74f1042480b74d1f271953421fd00ec99a2c5c
SanyaSriv <ssrivas7@ucsc.edu>
2024-11-16 16:10:37 -0500

    Adding funciton for failed and slow tasks

--- src/hadoop_sched.py
+++ src/hadoop_sched.py
@@ -5,6 +5,8 @@ This file contains the hadoop scheduler class.
 class NaiveScheduler:
     def __init__(self, node_cluster):
         self.node_cluster = node_cluster
+        self.failed_tasks = []
+        self.slow_tasks = []
 
     def assign_map_tasks(self):
         """Function to assign map tasks to workers."""
@@ -12,4 +14,10 @@ class NaiveScheduler:
 
     def assign_red_tasks(self):
         """Function to assign reduce tasks to workers."""
+        pass
+
+    def assign_failed_tasks(self):
+        pass
+
+    def assign_slow_tasks(self):
         pass
