bc1586457568ae792d02bac19dc896ecbe989fc2
SanyaSriv <ssrivas7@ucsc.edu>
2024-11-16 15:51:15 -0500

    Made some more functions

--- src/node.py
+++ src/node.py
@@ -5,14 +5,36 @@ that will execute the map/reduce tasks.
 """
 
 class NodeCluster:
-    def __init__(self, num_nodes):
+    def __init__(self, num_nodes, slow_N_Threshold):
         self.num_nodes = num_nodes
+        self.slow_N_Threshold = slow_N_Threshold
+
+    def make_homogeneous_nodes():
+        """
+        This function should make homogeneous self.num_nodes number of nodes.
+        Every node should have the same properties. 
+        """
+        pass
+
+    def make_heterogeneous_nodes(json_node_config):
+        """
+        This function would take a JSON node configuraion and would
+        make heterogenous nodes. The number of nodes in the configuration
+        should be equal to the self.num_nodes parameter.
+        """
+        pass
 
 #TODO: add all the correct arguments to these node classes.
 # As of now, I have just defined a basic skeleton structure. 
 class Node:
     def __init__(self):
-        pass
+        # initializing everything for now
+        self.progress_score = 0
+        self.time_to_completion = 0
+        self.progress_rate = 0
+
+    def update_time_to_completion(self):
+        self.time_to_completion = (1 - self.progress_score) / self.progress_rate
 
     def assign_map_task(self, mtask):
         pass
