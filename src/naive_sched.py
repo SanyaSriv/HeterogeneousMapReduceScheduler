c702023356dce7a14b19c476faee0abd0a24bb94
SanyaSriv <ssrivas7@ucsc.edu>
2024-11-21 13:50:10 -0500

    Adding all the files

--- src/naive_sched.py
+++ src/naive_sched.py
@@ -1,15 +1,15 @@
 """
-This file contains the hadoop scheduler class. 
+This file contains the naive scheduler class - that will do no specu. 
 """
 
-class HadoopScheduler:
+class NaiveScheduler:
     def __init__(self, node_cluster, tasks, threshold):
         self.node_cluster = node_cluster
         self.failed_tasks = [] # prioritized over slow tasks
         self.slow_tasks = []
         self.tasks = tasks
         self.threshold = threshold # to decide which task is slow
-        self.id = "hadoop"
+        self.id = "naive"
 
     def assign_map_tasks(self):
         """Function to assign map tasks to workers."""
