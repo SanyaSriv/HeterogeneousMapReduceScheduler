20e392a83188ef41daf0f6a88c1b9b8520ee11ab
Ning Liang <ning@Nings-MacBook-Air.local>
2024-11-24 02:26:37 -0500

    hadoop done

--- src/hadoop_sched.py
+++ src/hadoop_sched.py
@@ -1,60 +1,139 @@
 """
 This file contains the hadoop scheduler class. 
 """
+import threading
+import time
 
 class HadoopScheduler:
-    def __init__(self, node_cluster, tasks, threshold):
-        self.node_cluster = node_cluster
-        self.failed_tasks = [] # prioritized over slow tasks
-        self.slow_tasks = []
-        self.tasks = tasks
+    def __init__(self, tasks, threshold):
+        self.task_id = len(tasks) - 1
+        self.regular_tasks = {}
+        self.duplicate_tasks = {}
+        self.running_tasks = {}
+        self.regular_tasks = tasks
         self.threshold = threshold # to decide which task is slow
         self.id = "hadoop"
         self.map_tasks_remaining = []
         self.available_nodes = []
         self.node_progress_stats = {}
+        self.worker_threads = {}
+        self.lock_stats = threading.Lock()
+        self.lock_tasks = threading.Lock()
+        self.lock_nodes = threading.Lock()
+
+    
+    def set_node_cluster(self, node_cluster):
+        print("SET")
+        self.node_cluster = node_cluster
         for node_id in self.node_cluster.node_pool:
-            # TODO, I am not sure how to initialize these attributes
-            self.node_progress_stats[node_id] = {"working": False, # the nodes are not working by default
-                                                 "task_id": -1, # set it to the apt task id
-                                                "progress_score": 0,
-                                                "time_to_completion": 0,
-                                                "progress_rate": 0}
             self.available_nodes.append(node_id)
-            
+            self.node_progress_stats[node_id] = {"working": True, # the nodes are not working by default
+                                                 "task_id": -1, # set it to the apt task id
+                                                "progress_score": -1,
+                                                "time_to_completion": 0,
+                                                "progress_rate": 0,
+                                                "dup": 0}
+    
+    def update_node_progress(self, id, progress_score, T, task_id, dup):
+        """Function to change the progress stats of a node"""
+        with self.lock_stats:
+            self.node_progress_stats[id]["progress_score"] = progress_score
+            self.node_progress_stats[id]["task_id"] = task_id
+            self.node_progress_stats[id]["dup"] = dup
+
     def assign_tasks(self):
-        """Function to assign map tasks to workers."""
-        while (len(self.tasks) > 0):
-            for task in self.tasks:
-                if self.task[task]["type"] == "map":
-                    pass
-                    # we need to launch a thread
-                    # assign a job to one of the available workers
-                elif self.task[task]["type"] == "copy":
-                    pass
-                    # we need to launch a thread
-                    # assign a job to one of the available workers
-                elif self.task[task]["type"] == "sort":
-                    pass
-                    # we need to launch a thread
-                    # assign a job to one of the available workers
-                elif self.task[task]["type"] == "reduce":
-                    if len(self.map_tasks_remaining) > 0:
-                        continue
-                    pass
-                    # make sure all the map tasks have finished first
-                    # we need to launch a thread
-                    # assign a job to one of the workers
-            
-            # check if we have any late/failed tasks
-            if len(self.slow_tasks) > 0:
-                self.assign_slow_tasks()
-            if len(self.failed_tasks) > 0:
-                self.assign_failed_tasks()
+        print(self.available_nodes)
+        while(True):
+            if len(self.regular_tasks) != 0:
+                for tid in self.regular_tasks:
+                    task = self.regular_tasks[tid]
+                    if len(self.available_nodes) != 0:
+                        with self.lock_nodes:
+                            node_id = self.available_nodes.pop()
+                            worker = self.node_cluster.node_pool[node_id]
+                        if task["type"] == "map":
+                            print("Assign Map job")
+                            thread = threading.Thread(target=worker.execute_map_task, args=(tid,0))
+                            self.worker_threads[node_id] = thread
+                            thread.start()
+                        elif task["type"] == "reduce":
+                            print("Assign Reduce job")
+                            thread = threading.Thread(target=worker.execute_reduce_task, args=(tid,0))
+                            self.worker_threads[node_id] = thread
+                            thread.start()
+                        elif task["type"] == "copy":
+                            print("Assign Copy job")
+                            thread = threading.Thread(target=worker.execute_copy_task, args=(tid,0))
+                            self.worker_threads[node_id] = thread
+                            thread.start()
+                        elif task["type"] == "sort":
+                            print("Assign Sort job")
+                            thread = threading.Thread(target=worker.execute_sort_task, args=(tid,0))
+                            self.worker_threads[node_id] = thread
+                            thread.start()
+                        with self.lock_tasks:
+                            print("Pop task")
+                            self.regular_tasks.pop(tid)
+                            self.running_tasks[tid] = [task, 0]
+                    break
+            else:
+                if len(self.available_nodes) != 0:
+                    with self.lock_nodes:
+                        node_id = self.available_nodes.pop()
+                        worker = self.node_cluster.node_pool[node_id]
+                    for nid in self.node_progress_stats:
+                        if self.node_progress_stats[nid]["progress_score"] < self.threshold and self.node_progress_stats[nid]["task_id"] != -1:
+                
+                            tid = self.node_progress_stats[nid]["task_id"]
+                            if tid in self.duplicate_tasks:
+                                break
+                            task = self.running_tasks[tid][0]
+                            print("assigning dup task", self.running_tasks[tid], self.node_progress_stats[nid]["progress_score"])
+                            if task["type"] == "map":
+                                thread = threading.Thread(target=worker.execute_map_task, args=(tid,1))
+                                self.worker_threads[node_id] = thread
+                                thread.start()
+                            elif task["type"] == "reduce":
+                                thread = threading.Thread(target=worker.execute_reduce_task, args=(tid,1))
+                                self.worker_threads[node_id] = thread
+                                thread.start()
+                            elif task["type"] == "copy":
+                                thread = threading.Thread(target=worker.execute_copy_task, args=(tid,1))
+                                self.worker_threads[node_id] = thread
+                                thread.start()
+                            elif task["type"] == "sort":
+                                thread = threading.Thread(target=worker.execute_sort_task, args=(tid,1))
+                                self.worker_threads[node_id] = thread
+                                thread.start()
+                            with self.lock_tasks:
+                                self.duplicate_tasks[tid] = [task,0]
+                            break
+            if len(self.regular_tasks) == 0 and len(self.running_tasks) == 0:
+                break
 
 
-    def assign_failed_tasks(self):
-        pass
-
-    def assign_slow_tasks(self):
-        pass
+    def generate_next_tasks(self):
+        while(True):
+            for tid in self.running_tasks:
+                if self.running_tasks[tid][1] == 1 or (tid in self.duplicate_tasks and self.duplicate_tasks[tid][1] == 1):
+                        #TODO delete the two tasks, one of them finished
+                        time.sleep(2)     
+                        with self.lock_tasks:
+                            if self.running_tasks[tid][0]["type"] == "map":
+                                self.task_id += 1
+                                self.regular_tasks[self.task_id] = {"type": "copy"}
+                                print("Generating new copy tasks")
+                            elif self.running_tasks[tid][0]["type"] == "copy":
+                                self.task_id += 1
+                                self.regular_tasks[self.task_id] = {"type": "sort"}
+                                print("Generating new sort tasks")
+                            elif self.running_tasks[tid][0]["type"] == "sort":
+                                self.task_id += 1
+                                self.regular_tasks[self.task_id] = {"type": "reduce"}
+                                print("Generating new Red tasks")
+                            self.running_tasks.pop(tid)
+                            if tid in self.duplicate_tasks:
+                                self.duplicate_tasks.pop(tid)
+                break
+            if len(self.regular_tasks) == 0 and len(self.running_tasks) == 0:
+                break
