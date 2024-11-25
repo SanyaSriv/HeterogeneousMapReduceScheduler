e27ab598dcfa5d21b6a162947fb69af254da056f
SanyaSriv <ssrivas7@ucsc.edu>
2024-11-25 12:38:46 -0500

    Explicitly marking straggler nodes

--- src/hadoop_node.py
+++ src/hadoop_node.py
@@ -40,10 +40,12 @@ class NodeCluster:
         number_of_straglers = random.randint(1, int(self.num_nodes / 2))
         # generate which node IDs will become stragglers
         straggler_list = [random.randint(0, self.num_nodes-1) for _ in range(number_of_straglers)]
+        # print("number of stragglers: ", number_of_straglers, straggler_list)
         for i in range(0, self.num_nodes):
             rangeA = 1.5 # can adjust it later
             rangeB = 4 # can adjust it later
             if i in straggler_list:
+                form_log(f"STRAGGLER: [NODE:{i}]")
                 rangeA = 0.1 # can adjust it later
                 rangeB = 1.5 # can adjust it later
             self.node_pool[i] = Node(i, 
