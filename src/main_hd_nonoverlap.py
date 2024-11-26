7cc0e7f4f5ae0e54fb1fd439996a98f8e7c47e4b
Ning Liang <ning@Nings-MacBook-Air.local>
2024-11-26 03:06:13 -0500

    no overlap map reduce

---
+++ src/main_hd_nonoverlap.py
@@ -0,0 +1,31 @@
+import hadoop_wo_overlap
+import node_wo_overlap
+import threading
+# making a node cluster here
+node_cluster = node_wo_overlap.NodeCluster(6, 0.5, 10, 8, 6, 5)
+
+# defining the tasks here
+tasks = {"0": {"type": "map"},
+         "1": {"type": "map"},
+         "2": {"type": "map"},
+         "3": {"type": "map"},
+         "4": {"type": "map"},
+         "5": {"type": "map"}}
+
+# making the scheduler instance here
+sched = hadoop_wo_overlap.HadoopScheduler(tasks, 0.9)
+
+node_cluster.set_scheduler(sched)
+node_cluster.init_homogeneous_nodes(2, [0, 1])
+sched.set_node_cluster(node_cluster)
+
+
+# Create threads for both functions
+scheduler_thread = threading.Thread(target=sched.assign_tasks)
+gen_thread = threading.Thread(target=sched.generate_next_tasks)
+
+scheduler_thread.start()
+gen_thread.start()
+
+scheduler_thread.join()
+gen_thread.join()
