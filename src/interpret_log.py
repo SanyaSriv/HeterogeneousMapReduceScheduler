fc582be516ce84adeb05143d0e761763de7986db
SanyaSriv <ssrivas7@ucsc.edu>
2024-11-25 23:11:46 -0500

    Adding the progress score display

--- src/interpret_log.py
+++ src/interpret_log.py
@@ -4,7 +4,7 @@ import argparse
 import re
 
 # defining some global REs here that will help in sorting through the logs
-PARAM_MATCH =  r"\[([a-zA-Z]+):([0-9]+)\]"
+PARAM_MATCH =  r"\[([a-zA-Z]+):([0-9.]+)\]"
 
 def processing_logs_stage1(raw_log):
     sorted_logs = {"node": [],
@@ -24,6 +24,7 @@ def processing_logs_stage2(log):
     event_dictionary = {}
     for event in log:
         event_list = event.split(" ")
+        print("event list = ", event_list)
         time_stamp = event_list[0].replace("[", "").replace("]", "")
         event_type = event_list[2].replace(":", "")
         event_parameters = {}
@@ -93,15 +94,18 @@ def main():
         nodes_redundant = [int(x['params']['NODE']) for x in node_logs["ABORT-REDUNDANT-MAP"]]
         tasks_redundant = [int(x['params']['TASK']) for x in node_logs["ABORT-REDUNDANT-MAP"]]
         dup_redundant = [int(x['params']['DUP']) for x in node_logs["ABORT-REDUNDANT-MAP"]]
+        score_redundant = [float(x['params']['STAT']) for x in node_logs["ABORT-REDUNDANT-MAP"]]
     else:
         redundant_abort_timestamp = []
         nodes_redundant = []
         tasks_redundant = []
         dup_redundant = []
+        score_redundant = []
 
     time_stamp_dict_begin = {}
     time_stamp_dict_end = {}
     redundant_time_stamp_dict = {}
+    score = {}
 
     for i in range(0, len(nodes1)):
         time_stamp_dict_begin[(nodes1[i], tasks1[i], dup1[i])] = timestamps_begin[i]
@@ -111,23 +115,32 @@ def main():
     
     for i in range(0, len(nodes_redundant)):
         redundant_time_stamp_dict[(nodes_redundant[i], tasks_redundant[i], dup_redundant[i])] = redundant_abort_timestamp[i]
-    
+        score[(nodes_redundant[i], tasks_redundant[i], dup_redundant[i])] = score_redundant[i]
+
     plt.figure(figsize=(10, 6))
     for i in range(len(timestamps_begin)):
+        score_display = 0
         pair = (nodes1[i], tasks1[i], dup1[i])
         if pair in time_stamp_dict_end:
             time_end = time_stamp_dict_end[pair]
         elif pair in redundant_time_stamp_dict:
             time_end = redundant_time_stamp_dict[pair]
+            score_display = 1
         else:
             print("NO END TIME FOUND ----------")
         if dup1[i] == 1:
             # mark these duplicate tasks in red
-            c = "red"
+            c1 = "red"
+            c2 = "black"
         else:
-            c = "darkblue"
-        plt.barh(nodes1[i], time_end - timestamps_begin[i], left=timestamps_begin[i], color=c, edgecolor="black", height=0.4)
-        plt.text(timestamps_begin[i] + 0.004, nodes1[i], f"{tasks1[i]}", va='center', fontsize=12)
+            c1 = "darkblue"
+            c2 = "white"
+        if score_display == 0:
+            to_disp = tasks1[i]
+        else:
+            to_disp = "{}   --   {:.2f}".format(tasks1[i], score[pair])
+        plt.barh(nodes1[i], time_end - timestamps_begin[i], left=timestamps_begin[i], color=c1, edgecolor="black", height=0.4)
+        plt.text(timestamps_begin[i] + 0.004, nodes1[i], to_disp, va='center', fontsize=12, color=c2)
     # plt.xlabel("Timestamp")
     # plt.ylabel("Node ID")
     # plt.title("Map Task Begin Timeline (Straggler Nodes Highlighted)")
@@ -158,15 +171,18 @@ def main():
         cpy_nodes_redundant = [int(x['params']['NODE']) for x in node_logs["ABORT-REDUNDANT-COPY"]]
         cpy_tasks_redundant = [int(x['params']['TASK']) for x in node_logs["ABORT-REDUNDANT-COPY"]]
         cpy_dup_redundant = [int(x['params']['DUP']) for x in node_logs["ABORT-REDUNDANT-COPY"]]
+        score_redundant = [float(x['params']['STAT']) for x in node_logs["ABORT-REDUNDANT-COPY"]]
     else:
         cpy_redundant_abort_timestamp = []
         cpy_nodes_redundant = []
         cpy_tasks_redundant = []
         cpy_dup_redundant = []
+        score_redundant = []
 
     cpy_time_stamp_dict_begin = {}
     cpy_time_stamp_dict_end = {}
     cpy_redundant_time_stamp_dict = {}
+    score = {}
 
     for i in range(0, len(cpy_nodes1)):
         cpy_time_stamp_dict_begin[(cpy_nodes1[i], cpy_tasks1[i], cpy_dup1[i])] = cpy_timestamps_begin[i]
@@ -176,17 +192,20 @@ def main():
     
     for i in range(0, len(cpy_nodes_redundant)):
         cpy_redundant_time_stamp_dict[(cpy_nodes_redundant[i], cpy_tasks_redundant[i], cpy_dup_redundant[i])] = cpy_redundant_abort_timestamp[i]
-
+        score[(nodes_redundant[i], tasks_redundant[i], dup_redundant[i])] = score_redundant[i]
+    
     print(cpy_time_stamp_dict_begin)
     print(cpy_time_stamp_dict_end)
     print(cpy_redundant_time_stamp_dict)
     # plt.figure(figsize=(10, 6))
     for i in range(len(cpy_timestamps_begin)):
+        score_display = 0
         pair = (cpy_nodes1[i], cpy_tasks1[i], cpy_dup1[i])
         if pair in cpy_time_stamp_dict_end:
             time_end = cpy_time_stamp_dict_end[pair]
         elif pair in cpy_redundant_time_stamp_dict:
             time_end = cpy_redundant_time_stamp_dict[pair]
+            score_display = 1
         else:
             print("NO END TIME FOUND FOR COPY ----------")
         if cpy_dup1[i] == 1:
@@ -194,8 +213,12 @@ def main():
             c = "red"
         else:
             c = "skyblue"
+        if score_display == 0:
+            to_disp = tasks1[i]
+        else:
+            to_disp = "{}   --   {:.2f}".format(tasks1[i], score[pair])
         plt.barh(cpy_nodes1[i], time_end - cpy_timestamps_begin[i], left=cpy_timestamps_begin[i], color=c, edgecolor="black", height=0.4)
-        plt.text(cpy_timestamps_begin[i] + 0.004, cpy_nodes1[i], f"{cpy_tasks1[i]}", va='center', fontsize=12)
+        plt.text(cpy_timestamps_begin[i] + 0.004, cpy_nodes1[i], to_disp, va='center', fontsize=12)
     
     
     # computing the same things for sort because the copy - sort - and reduce phases are combined into 1 task
@@ -217,16 +240,19 @@ def main():
         sort_nodes_redundant = [int(x['params']['NODE']) for x in node_logs["ABORT-REDUNDANT-SORT"]]
         sort_tasks_redundant = [int(x['params']['TASK']) for x in node_logs["ABORT-REDUNDANT-SORT"]]
         sort_dup_redundant = [int(x['params']['DUP']) for x in node_logs["ABORT-REDUNDANT-SORT"]]
+        score_redundant = [float(x['params']['STAT']) for x in node_logs["ABORT-REDUNDANT-SORT"]]
     else:
         sort_redundant_abort_timestamp = []
         sort_nodes_redundant = []
         sort_tasks_redundant = []
         sort_dup_redundant = []
+        score_redundant = []
     
     # making sort timestamp dictionaries in here
     sort_time_stamp_dict_begin = {}
     sort_time_stamp_dict_end = {}
     sort_redundant_time_stamp_dict = {}
+    score = {}
 
     for i in range(0, len(sort_nodes1)):
         sort_time_stamp_dict_begin[(sort_nodes1[i], sort_tasks1[i], sort_dup1[i])] = sort_timestamps_begin[i]
@@ -236,13 +262,16 @@ def main():
 
     for i in range(0, len(sort_nodes_redundant)):
         sort_redundant_time_stamp_dict[(sort_nodes_redundant[i], sort_tasks_redundant[i], sort_dup_redundant[i])] = sort_redundant_abort_timestamp[i]
+        score[(nodes_redundant[i], tasks_redundant[i], dup_redundant[i])] = score_redundant[i]
 
     for i in range(0, len(sort_timestamps_begin)):
+        score_display = 0
         pair = (sort_nodes1[i], sort_tasks1[i], sort_dup1[i])
         if pair in sort_time_stamp_dict_end:
             time_end = sort_time_stamp_dict_end[pair]
         elif pair in sort_redundant_time_stamp_dict:
             time_end = sort_redundant_time_stamp_dict[pair]
+            score_display = 1
         else:
             print("NO END TIME FOUND FOR SORT PHASE")
         if sort_dup1[i] == 1:
@@ -250,8 +279,12 @@ def main():
             c = "#ed8e98"
         else:
             c = "#bfe01b" 
+        if score_display == 0:
+            to_disp = tasks1[i]
+        else:
+            to_disp = "{}   --   {:.2f}".format(tasks1[i], score[pair])
         plt.barh(sort_nodes1[i], time_end - sort_timestamps_begin[i], left=sort_timestamps_begin[i], color=c, edgecolor="black", height=0.4)
-        plt.text(sort_timestamps_begin[i]+0.004, sort_nodes1[i], f"{sort_tasks1[i]}", va="center", fontsize=12)
+        plt.text(sort_timestamps_begin[i]+0.004, sort_nodes1[i], to_disp, va="center", fontsize=12)
     
     # adding data about reduce tasks here
     red_timestamps_begin = [float(x['timestamp']) for x in node_logs["BEGIN-RED"]]
@@ -271,16 +304,19 @@ def main():
         red_nodes_redundant = [int(x['params']['NODE']) for x in node_logs["ABORT-REDUNDANT-RED"]]
         red_tasks_redundant = [int(x['params']['TASK']) for x in node_logs["ABORT-REDUNDANT-RED"]]
         red_dup_redundant = [int(x['params']['DUP']) for x in node_logs["ABORT-REDUNDANT-RED"]]
+        score_redundant = [float(x['params']['STAT']) for x in node_logs["ABORT-REDUNDANT-RED"]]
     else:
         red_redundant_abort_timestamp = []
         red_nodes_redundant = []
         red_tasks_redundant = []
         red_dup_redundant = []
+        score_redundant = []
     
     # making sort timestamp dictionaries in here
     red_time_stamp_dict_begin = {}
     red_time_stamp_dict_end = {}
     red_redundant_time_stamp_dict = {}
+    score = {}
 
     for i in range(0, len(red_nodes1)):
         red_time_stamp_dict_begin[(red_nodes1[i], red_tasks1[i], red_dup1[i])] = red_timestamps_begin[i]
@@ -290,16 +326,19 @@ def main():
 
     for i in range(0, len(red_nodes_redundant)):
         red_redundant_time_stamp_dict[(red_nodes_redundant[i], red_tasks_redundant[i], red_dup_redundant[i])] = red_redundant_abort_timestamp[i]
+        score[(nodes_redundant[i], tasks_redundant[i], dup_redundant[i])] = score_redundant[i]
 
     print(red_time_stamp_dict_begin)
     print(red_time_stamp_dict_end)
     print(red_redundant_time_stamp_dict)
     for i in range(0, len(red_timestamps_begin)):
         pair = (red_nodes1[i], red_tasks1[i], red_dup1[i])
+        score_display = 0
         if pair in red_time_stamp_dict_end:
             time_end = red_time_stamp_dict_end[pair]
         elif pair in red_redundant_time_stamp_dict:
             time_end = red_redundant_time_stamp_dict[pair]
+            score_display = 1
         else:
             print("NO END TIME FOUND FOR RED PHASE: ", pair)
         if red_dup1[i] == 1:
@@ -307,8 +346,12 @@ def main():
             c = "#f2c2c7"
         else:
             c = "yellow" 
+        if score_display == 0:
+            to_disp = tasks1[i]
+        else:
+            to_disp = "{}   --   {:.2f}".format(tasks1[i], score[pair])
         plt.barh(red_nodes1[i], time_end - red_timestamps_begin[i], left=red_timestamps_begin[i], color=c, edgecolor="black", height=0.4)
-        plt.text(red_timestamps_begin[i]+0.004, red_nodes1[i], f"{red_tasks1[i]}", va="center", fontsize=12)
+        plt.text(red_timestamps_begin[i]+0.004, red_nodes1[i], to_disp, va="center", fontsize=12)
     
     # collecting all timestamps when a red generation happens
     red_gen_timestamps = [float(x['timestamp']) for x in sched_logs["GEN-RED"]]
